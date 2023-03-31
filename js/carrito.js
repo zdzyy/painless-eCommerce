f8fed05ed7b5ec94a84aa53b69c716063e4ea4c5
Diego Mendoza <119355955+zdzyy@users.noreply.github.com>
2023-03-31 12:43:57 -0600

    carrito js addons

--- js/carrito.js
+++ js/carrito.js
@@ -1,8 +1,9 @@
 const productosEnCarrito = JSON.parse(localStorage.getItem("productos-en-carrito"));
 
 const contenedorCarritoVacio = document.querySelector("#carrito-vacio")
-const contenedorProductos = document.querySelector("#carrito-vacio")
-const carritoVacio = document.querySelector("#carrito-vacio")
+const contenedorProductos = document.querySelector("#carrito-productos")
+const carritoCarritoAcciones = document.querySelector("#carrito-acciones")
+const contenedorCarritoComprado = document.querySelector("#carrito-comprado")
 
 if (productosEnCarrito) {
 
