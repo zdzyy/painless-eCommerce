f5d5acbd267f6fab305f2b00e2480e7f4134764c
Diego Mendoza <119355955+zdzyy@users.noreply.github.com>
2023-03-31 12:46:04 -0600

    Update carrito.js

--- js/carrito.js
+++ js/carrito.js
@@ -1,12 +1,15 @@
 const productosEnCarrito = JSON.parse(localStorage.getItem("productos-en-carrito"));
 
 const contenedorCarritoVacio = document.querySelector("#carrito-vacio")
-const contenedorProductos = document.querySelector("#carrito-productos")
-const carritoCarritoAcciones = document.querySelector("#carrito-acciones")
+const contenedorCarritoProductos = document.querySelector("#carrito-productos")
+const contenedorCarritoAcciones = document.querySelector("#carrito-acciones")
 const contenedorCarritoComprado = document.querySelector("#carrito-comprado")
 
 if (productosEnCarrito) {
-
+    contenedorCarritoVacio.classList.add("disabled");
+    contenedorCarritoProductos.classList.remove("disabled");
+    contenedorCarritoAcciones.classList.remove("disabled");
+    contenedorCarritoComprado.classList.add("disabled");
 } else {
 
 }
