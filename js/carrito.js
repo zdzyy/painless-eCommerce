d883589864ff8c59a58d471cbd62a6a330b6a557
Diego Mendoza <119355955+zdzyy@users.noreply.github.com>
2023-03-31 12:55:38 -0600

    update

--- js/carrito.js
+++ js/carrito.js
@@ -10,6 +10,31 @@ if (productosEnCarrito) {
     contenedorCarritoProductos.classList.remove("disabled");
     contenedorCarritoAcciones.classList.remove("disabled");
     contenedorCarritoComprado.classList.add("disabled");
+
+    productosEnCarrito.forEach(producto => {
+        const div = document.createElement("div");
+        div.classList.add("carrito-producto");
+        div.innerHTML = `
+            <img class="carrito__producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+            <div class="carrito__producto-titulo">
+                <small>nombre</small>
+                <h3>painless hoodie</h3>
+            </div>
+            <div class="carrito__producto-cantidad">
+                <small>cantidad</small>
+                <p>1</p>
+            </div>
+            <div class="carrito__producto-precio">
+                <small>precio</small>
+                <p>$849.99</p>
+            </div>
+            <div class="carrito__producto-subtotal">
+                <small>subtotal</small>
+                <p>$849.99</p>
+            </div>
+            <button class="carrito__producto-eliminar"><i class="bi bi-trash-fill"></i></button>
+        `
+    });
 } else {
 
 }
