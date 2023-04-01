152c685a560c0b53211a4308910d9fd1e98c32a9
Diego Mendoza <119355955+zdzyy@users.noreply.github.com>
2023-04-01 16:49:36 -0600

    stuck

--- js/script.js
+++ js/script.js
@@ -1,33 +1,22 @@
 // PRODUCTOS
-const productos = [
-    // hoodies
-    {
-        id: "nintendo",
-        titulo: "Nintendo hoodie",
-        imagen: "assets/nintendohoodieBack.png",
-        precio: 1499.99
-    },
-    {
-        id: "painless",
-        titulo: "Painless hoodie",
-        imagen: "assets/painlesshodieeBack.png",
-        precio: 849.99
-    },
-    {
-        id: "rndm",
-        titulo: "RNDM XCLSV",
-        imagen: "assets/rndmhoodie.png",
-        precio: 1299.99
-    },
-];
+let productos = [];
+fetch("./js/productos.json")
+    .then(response => response.json())
+    .then(data => {
+        productos = data;
+        cargarProductos(productos);
+    })
 
 const contenedorProductos = document.querySelector("#contenedor-productos");
 let botonesAgregar = document.querySelectorAll(".card__add");
 const numerito = document.querySelector("#numerito");
 
 function cargarProductos(productosElegidos) {
+
     contenedorProductos.innerHTML = "";
+
     productosElegidos.forEach(producto => {
+
         const div = document.createElement("div");
         div.classList.add("card")
         div.innerHTML = `
@@ -38,6 +27,7 @@ function cargarProductos(productosElegidos) {
 		        <button class="card__add" id="${producto.id}">agregar</button>
 	        </div>
         `;
+
         contenedorProductos.append(div);
     })
 
@@ -47,12 +37,16 @@ function cargarProductos(productosElegidos) {
 
 function actualizarBotonesAgregar() {
     botonesAgregar = document.querySelectorAll(".card__add");
+
     botonesAgregar.forEach(boton => {
         boton.addEventListener("click", agregarAlCarrito);
     });
 }
+
 let productosEnCarrito;
+
 let productosEnCarritoLS = localStorage.getItem("productos-en-carrito");
+
 if (productosEnCarritoLS) {
     productosEnCarrito = JSON.parse(productosEnCarritoLS);
     actualizarNumerito();
@@ -71,7 +65,9 @@ function agregarAlCarrito(e) {
         productoAgregado.cantidad = 1;
         productosEnCarrito.push(productoAgregado);
     }
+
     actualizarNumerito();
+
     localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
 }
 
